1f64ccf687c0430ffeb6532d8b1958deeb4253de
Chris Mems <christopher.mems@.com>
2023-03-27 21:02:03 -0500

    set up user model

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from './controllers/auth.js'
 
 // Configurations
 const __filename = fileURLToPath(import.meta.url);
@@ -35,6 +36,9 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage });
+
+// Routes With Files
+app.post('/auth/register', upload.single('picture'), register)
 
 // Mongoose Setup
 const PORT = process.env.PORT || 6001;
